7116ca97445017f1248e88b3d9c3d14bd476bd33
Sara Torrey <sara.moravej@gmail.com>
2019-06-15 06:54:47 -0500

    Add animal sounds and repeat routes.

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -2,23 +2,37 @@ var express = require( "express" );
 var app = express();
 
 app.get( "/", function ( req, res ) {
-    res.send( "I'm Sara!" );
+    res.send( "Hi! Welcome to my exercise!" );
 } );
 
-app.get( "/bye", function ( req, res ) {
-    res.send( "Baaaaai!" );
+app.get( "/speak/:animal", function ( req, res ) {
+    var sound = {
+        pig: "oink",
+        cow: "moo",
+        dog: "woof",
+        frog: "rebbit",
+        duck: "quack"
+};
+
+    var animal = req.params.animal.toLowerCase();
+    var sound = sound [animal];
+    res.send( "The " + animal + " says  " + sound );
 } );
 
-
-app.get( "/kitty", function ( req, res ) {
-    console.log( "cat" );
-    res.send( "meow!" );
+app.get( "/", function ( req, res ) {
+    res.send( "Hi! Welcome to my exercise!" );
 } );
 
-app.get( "*", function ( req, res ) {
-    res.send( "Invalid Page!" );
-} );
+app.get( "/repeat/:message/:times", function ( req, res ) {
+    var message = req.params.message;
+    var times = Number(req.params.times);
+    var result = "";
 
+    for ( var i = 0; i < times; i++ ) {
+        result += message + " ";
+    }
+    res.send( result );
+} );
 
 app.listen(3000, process.env.IP, function (  ) {
     console.log("Server has started.")
