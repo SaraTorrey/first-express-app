d53315e3e4737577c32a835a3750070586358413
Sara Torrey <sara.moravej@gmail.com>
2019-06-17 15:08:07 -0500

    Add posts.ejs

--- FirstExpressApp/MoreExpress/EJSDemo/app.js
+++ FirstExpressApp/MoreExpress/EJSDemo/app.js
@@ -10,6 +10,15 @@ app.get( "/fallinlovewith/:thing", function ( req, res ) {
     res.render( "love.ejs", {thingVar: thing});
 } );
 
+app.get( "/posts", function ( req, res ) {
+    let posts = [
+        {title: "Post 1", author: "Sara"},
+        {title: "Funny Bunny", author: "Shabnam"},
+        {title: "Super Cute Cat", author: "Susan"}
+    ];
+    res.render("posts.ejs", {posts: posts})
+} );
+
 app.listen(3000, process.env.IP, function (  ) {
     console.log("Server is listening!")
 });
