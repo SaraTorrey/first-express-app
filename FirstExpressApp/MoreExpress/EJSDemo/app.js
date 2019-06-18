09755d3243cd04c38a7db5b146e32ff6e7bcd9ff
Sara Torrey <sara.moravej@gmail.com>
2019-06-18 06:23:40 -0500

    Add post req Make a new page: friends.ejs Make redirect from /friends to /addfriend back to /friends

--- FirstExpressApp/MoreExpress/EJSDemo/app.js
+++ FirstExpressApp/MoreExpress/EJSDemo/app.js
@@ -1,11 +1,26 @@
 var express = require("express");
 var app = express();
+var bodyParser = require( "body-parser" );
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use( express.static( "public" ) );
 app.set( "view engine", "ejs" );
 
+let friends = ["Liz", "Mandy", "Pam", "Kim"];
+
 app.get( "/", function ( req, res ) {
     res.render( "home" );
+} );
+
+app.post( "/addfriend", function ( req, res ) {
+    var newFriend = req.body.newfriend ;
+    friends.push( newFriend );
+    res.redirect( "/friends" );
+} );
+
+app.get( "/friends", function ( req, res ) {
+    res.render( "friends", {friends : friends} );
 } );
 
 app.get( "/fallinlovewith/:thing", function ( req, res ) {
