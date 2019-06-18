6f1ba4a6439f6733f8b17d1b57bca903dc31424d
Sara Torrey <sara.moravej@gmail.com>
2019-06-18 05:26:38 -0500

    Create header.ejs and footer.ejs

--- FirstExpressApp/MoreExpress/EJSDemo/app.js
+++ FirstExpressApp/MoreExpress/EJSDemo/app.js
@@ -2,9 +2,10 @@ var express = require("express");
 var app = express();
 
 app.use( express.static( "public" ) );
+app.set( "view engine", "ejs" );
 
 app.get( "/", function ( req, res ) {
-    res.render( "home.ejs" );
+    res.render( "home" );
 } );
 
 app.get( "/fallinlovewith/:thing", function ( req, res ) {
@@ -18,7 +19,7 @@ app.get( "/posts", function ( req, res ) {
         {title: "Funny Bunny", author: "Shabnam"},
         {title: "Super Cute Cat", author: "Susan"}
     ];
-    res.render("posts.ejs", {posts: posts})
+    res.render("posts", {posts: posts})
 } );
 
 app.listen(3000, process.env.IP, function (  ) {
